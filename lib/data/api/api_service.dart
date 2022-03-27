3b96e90162578f173a0296c1f55ea8267037d48e
rifqi <rifqilutfi46@gmail.com>
2022-03-27 15:43:37 +0100

    Initial commit

---
+++ lib/data/api/api_service.dart
@@ -0,0 +1,19 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+import '../model/article.dart';
+
+class ApiService {
+  static final String _baseUrl = 'https://newsapi.org/v2/';
+  static final String _apiKey = '184c8366e0404945a494c6201ae233ee';
+  static final String _category = 'business';
+  static final String _country = 'id';
+
+  Future<ArticleResult> topHeadLines() async {
+    final response = await http.get(Uri.parse(_baseUrl + "top-headlines?country=$_country&category=$_category&apiKey=$_apiKey"));
+    if (response.statusCode == 200) {
+      return ArticleResult.fromJson(json.decode(response.body));
+    } else {
+      throw Exception('Failed to load top headlines');
+    }
+  }
+}
